da7a393c485744d919f2c981cd81ae7c1abf5362
mihael <bolsunovskiy@gmail.com>
2014-02-25 11:09:38 +0200

    update user admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from dashboard.models import Color
 from pyplanner.models import Config
+from django.contrib.auth.models import User
 
 
 class ColorAdmin(admin.ModelAdmin):
@@ -11,5 +12,12 @@ class ConfigAdmin(admin.ModelAdmin):
     list_display = ('key', 'value')
 
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_superuser',
+                    'is_active', 'last_login', 'date_joined')
+
+
 admin.site.register(Color, ColorAdmin)
 admin.site.register(Config, ConfigAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
